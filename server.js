07b15bdced84ef918c4e235c98a919a14128da41
Michael Cui <cuimichael500@gmail.com>
2020-07-14 21:29:54 -0500

    rearranging folders

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const fs = require("fs");
 
 //Port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
