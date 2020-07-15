873da768f91867d5d5476e5fe622d37874056998
Michael Cui <cuimichael500@gmail.com>
2020-07-14 20:14:24 -0500

    Added POST Method with functioning unique ids

--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,10 @@
+//Require all necessary components
 const express = require("express");
 const app = express();
 const path = require("path");
 const fs = require("fs");
 
+//Port
 const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
@@ -20,15 +22,9 @@ app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
     });
 
-//Routes to get to API
+//Routes to get to API notes
 app.get("/api/notes", function(req, res) {
   res.sendFile(path.join(__dirname, "/db/db.json"));
-});
-
-//Route to get Notes ID
-app.get("/api/notes/:id", function(req, res) {
-    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    res.json(savedNotes[Number(req.params.id)]);
 });
 
 //Route to get index.html
@@ -37,6 +33,28 @@ app.get("*", function(req, res) {
   });
   
 
+
+//Post API to save JSON data
+//Need to give these notes a unique id each time
+app.post("/api/notes", function(req, res) {
+
+  // Parses through the object
+let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+
+//Add the new note to the array
+let notesNew = req.body;
+notes.push(notesNew);
+
+  //for loop to add unique ids
+for (var i = 0;i<notes.length;i++){
+  notes[i].uniqueId = i;
+}
+
+  //Writes the JSON data back to db.json
+  fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+  console.log("Note saved to db.json. Content: ", notesNew);
+  res.json(notes);
+})
 
 
 
